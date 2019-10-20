2bcd944917bd823aa55495a54c3632f84abdc203
Tomas Ilginis <tilginis@gmail.com>
2019-10-20 21:58:01 +0300

    adds tests for hover dropdown case

--- src/components/navigation-items-desktop/navigation-items-desktop.e2e.ts
+++ src/components/navigation-items-desktop/navigation-items-desktop.e2e.ts
@@ -27,4 +27,39 @@ describe('rg-navigation', () => {
     const element = await page.find('rg-navigation-items-desktop');
     expect(element).toHaveClass('hydrated');
   });
+
+  it('displays dropdown menu on hover', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(`<rg-navigation-items-desktop></rg-navigation-items-desktop>`);
+
+    await page.$eval('rg-navigation-items-desktop', (elm: any) => {
+      const dataSet = [
+        {
+          name: "smth",
+          link: "localhost",
+          children: [
+            {
+              name: "child",
+              link: "localhost"
+            }
+          ],
+        }
+      ];
+      elm.navigationFields = dataSet;
+    });
+
+    await page.waitForChanges();
+
+    const mainMenuElementsContainer = await page.find('rg-navigation-items-desktop >>> .main-fields');
+    const elementToHover = await mainMenuElementsContainer.find('rg-navigation-item');
+
+    await elementToHover.hover();
+
+    const dropdownContainer = await page.find('rg-navigation-items-desktop >>> rg-navigation-dropdown-desktop');
+    const innerDropDownElement = await page.find('rg-navigation-items-desktop >>> rg-navigation-dropdown-desktop >>> rg-navigation-item');
+
+    expect(dropdownContainer).toHaveClass('dropdown-visible');
+    expect(innerDropDownElement).toHaveClass('hydrated');
+  })
 });
