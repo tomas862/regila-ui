d00ec8b3469f2824f793fe6619ce5bd2bfe4657a
Tomas Ilginis <tilginis@gmail.com>
2019-10-20 17:29:07 +0300

    added package improved some tests

--- src/components/anniversary/anniversary.e2e.ts
+++ src/components/anniversary/anniversary.e2e.ts
@@ -5,7 +5,20 @@ describe('rg-navigation', () => {
     const page = await newE2EPage();
 
     await page.setContent('<rg-anniversary></rg-anniversary>');
+
+    await page.$eval('rg-anniversary', (elm: any) => {
+
+      elm.date = '2004-02-20';
+      elm.name = 'happy anniversary';
+    });
+
+    await page.waitForChanges();
+
     const element = await page.find('rg-anniversary');
     expect(element).toHaveClass('hydrated');
+
+    const textElement = await page.find('rg-anniversary >>> .anniversary-title');
+    expect(textElement).toEqualHtml('<span class="anniversary-title"><div>happy</div><div>anniversary</div></span>');
+
   });
 });
--- src/components/navigation-items-desktop/navigation-items-desktop.e2e.ts
+++ src/components/navigation-items-desktop/navigation-items-desktop.e2e.ts
@@ -1,12 +1,29 @@
 import { newE2EPage } from '@stencil/core/testing';
 
 describe('rg-navigation', () => {
-  const dataSet = `[{"name": "smth", "link": "localhost", "children": [{"name": "child", "link": "localhost"}]}]`;
-
   it('renders', async () => {
     const page = await newE2EPage();
 
-    await page.setContent(`<rg-navigation-items-desktop navigation-fields="${dataSet}"></rg-navigation-items-desktop>`);
+    await page.setContent(`<rg-navigation-items-desktop></rg-navigation-items-desktop>`);
+
+    await page.$eval('rg-navigation-items-desktop', (elm: any) => {
+      const dataSet = [
+        {
+          name: "smth",
+          link: "localhost",
+          children: [
+            {
+              name: "child",
+              link: "localhost"
+            }
+          ],
+        }
+      ];
+      elm.navigationFields = dataSet;
+    });
+
+    await page.waitForChanges();
+
     const element = await page.find('rg-navigation-items-desktop');
     expect(element).toHaveClass('hydrated');
   });
