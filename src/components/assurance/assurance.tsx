2cae57042535bc7cb9285bbd54c0e6623b1477d0
Stanislovas <st.ramanauskas@gmail.com>
2019-11-09 13:41:49 +0200

    created asuurance component

---
+++ src/components/assurance/assurance.tsx
@@ -0,0 +1,17 @@
+import {Component, h} from "@stencil/core";
+
+@Component({
+  tag: 'rg-assurance',
+  styleUrl: 'assurance.scss',
+  shadow: true
+})
+export class Assurance {
+
+  render() {
+    return (
+      <div>
+        <div></div>
+      </div>
+    )
+  }
+}
