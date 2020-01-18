24e80a5fe9d4c776cf0255550a62a207b5ff7604
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-18 15:20:35 +0000

    Deploy tomas862/regila-ui to github.com/tomas862/regila-ui.git:gh-pages

--- build/regila-ui.js
+++ build/regila-ui.js
@@ -864,7 +864,7 @@ if (!win.__stencil_cssshim && needsShim()) {
 
     var resourcesUrl = scriptElm ? scriptElm.getAttribute('data-resources-url') || scriptElm.src : '';
     var start = function() {
-      var url = new URL('./p-c2e334b6.system.js', resourcesUrl);
+      var url = new URL('./p-eacefbf7.system.js', resourcesUrl);
       System.import(url.href);
     };
 
